e2693ba43a5076ff0820938b78e237bed27c641e
Deyan Vladkov <deian.vladkov@gmail.com>
2017-02-13 00:38:41 +0200

    first commit

---
+++ src/js/quotewall.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".quote-button").click(function () {
+        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote&?callback=?");
+    });
+});
+
+function parseQuote(response) {
+    var quote = response.quoteText;
+    var author = response.quoteAuthor;
+    quote = quote.trim();
+    author = author.trim();
+    $("#quote-text").html(quote);
+    $("#quote-author").html(author);
+    prepareTweet(quote, author);
+}
+
+function prepareTweet(quote, author) {
+    var path = window.location + '&text=' + '"' + quote + '" - ' + author;
+    var tweeturl = 'http://twitter.com/share?url=' + encodeURI(path);
+    $('#quote-tweet').html('<a id="tweet-url" href=" ' + tweeturl + ' ">Tweet Quote</a>');
+    $("#tweet-url").addClass("tweet-link");
+}
