d22a21e9750b07835fa64a277a945060782b75e9
Deyan Vladkov <deian.vladkov@gmail.com>
2017-03-05 17:55:13 +0200

    Deferred js load. Image Optimization.

--- src/js/quotewall.js
+++ src/js/quotewall.js
@@ -1,8 +1,32 @@
-$(document).ready(function () {
-    $(".quote-button").click(function () {
-        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote&?callback=?");
+(function () {
+    function loadScript(url, callback) {
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+        if (script.readyState) { //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        }
+        else { //Other Browsers
+            script.onload = function () {
+                callback();
+            };
+        }
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+    loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js", function () {
+        //jQuery loaded
+        $(document).ready(function () {
+            $(".quote-button").click(function () {
+                $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote&?callback=?");
+            });
+        });
     });
-});
+})();
 
 function parseQuote(response) {
     var quote = response.quoteText;
